3730a82c55179fff663f787a90a57e82998da3f3
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-02-26 21:27:29 +0000

    fixed all failed test cases

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ get("/square_root/new") do
 end
 
 get("/square_root/results") do
-  @user_input = params.fetch("square_root_number").to_f
-  @square_root = Math.sqrt(@user_input)
+  @user_input_square = params.fetch("square_root_number")
+  @square_root = Math.sqrt(@user_input_square.to_f)
   erb(:_1square_root_result)
 end
 
@@ -42,11 +42,11 @@ end
 
 get("/payment/results") do
   @user_apr = params.fetch("user_apr").to_f
-  @user_years = params.fetch("user_years").to_i
+  @user_years = params.fetch("user_years")
   @user_principal = params.fetch("user_principal").to_f
 
   @r = (@user_apr/100) / 12
-  @n = @user_years * 12
+  @n = @user_years.to_i * 12
 
   @numerator = @r * @user_principal
   @denominator = 1 - ((1 + @r) ** -@n)
@@ -54,7 +54,7 @@ get("/payment/results") do
   @payment = @numerator/@denominator
 
   @fromatted_apr = "#{"%.4f" % @user_apr}%"
-  @fromatted_principal = "$#{"%.2f" % @user_principal}" 
-  @formatted_payment = "$#{'%.2f' % @payment}"
+  @fromatted_principal = format("%.2f", @user_principal).reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+  @formatted_payment = format("%.2f", @payment).reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   erb(:_3payment_result)
 end
